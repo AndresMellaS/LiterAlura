07f466410e1d19a50beb3191ae107182acb3bd9b
AndresMellaS <daniel.sepulveda53746@edu.ipchile.cl>
2026-03-10 22:44:45 -0300

    fix: corregir @RequestMapping en AutorController y actualizar capturas

--- src/main/java/com/example/literalura/controller/AutorController.java
+++ src/main/java/com/example/literalura/controller/AutorController.java
@@ -2,6 +2,7 @@ package com.example.literalura.controller;
 
 import com.example.literalura.model.Autor;
 import com.example.literalura.service.AutorService;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -10,32 +11,42 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/autores")
 public class AutorController {
-
     private final AutorService autorService;
 
     public AutorController(AutorService autorService) {
         this.autorService = autorService;
     }
-
     @GetMapping
-    public ResponseEntity<List<Autor>> listarTodos() {
+    public ResponseEntity<List<Autor>> listarTodos(){
         List<Autor> autores = autorService.listarTodosLosAutores();
-        if (autores.isEmpty()) {
+        if (autores.isEmpty()){
             return ResponseEntity.noContent().build(); // 204
         }
-        return ResponseEntity.ok(autores); // 200
+        return ResponseEntity.ok(autores); //200
     }
 
     @GetMapping("/vivos/{año}")
-    public ResponseEntity<?> listarVivosEnAño(@PathVariable Integer año) {
-        if (año < 0 || año > 2025) {
+    public ResponseEntity<?> listarVivosEnAño (@PathVariable Integer  año){
+        if(año < 0 || año > 2026){
             return ResponseEntity.badRequest()
-                    .body(Map.of("error", "Ingresa un año válido entre 0 y 2025.")); // 400
+                    .body(Map.of("error", "Ingresa un año válido entre 0 y 2026")); //400
         }
         List<Autor> autores = autorService.listarAutoresVivosEnAño(año);
-        if (autores.isEmpty()) {
-            return ResponseEntity.noContent().build(); // 204
+        if(autores.isEmpty()){
+            return ResponseEntity.noContent().build(); //204
         }
-        return ResponseEntity.ok(autores); // 200
+        return ResponseEntity.ok(autores); //200
     }
+
+
+
+
+
+
+
+
+
+
 }
+
+
